48377e0547439c65d24867c0619578cdb9fb4684
axelgautier-PM <axelgautier94@gmail.com>
2026-03-25 14:25:28 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
   },
   icons: {
     apple: [
+      { url: "/icons/apple-touch-icon.png", sizes: "180x180" },
       { url: "/icons/icon-192.png", sizes: "192x192" },
-      { url: "/icons/icon-512.png", sizes: "512x512" },
     ],
   },
 };
